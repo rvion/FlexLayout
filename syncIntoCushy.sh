dbf4caeb4365c120a430048e53d2e7f1aa0c0751
rvion <vion.remi@gmail.com>
2025-03-16 23:02:38 +0100

    improve syncIntoCushy

--- syncIntoCushy.sh
+++ syncIntoCushy.sh
@@ -1,6 +1,7 @@
 # send with rsync the whole src/ folder into /Users/loco/dev/CushyStudio/src/FlexLayout/
 rsync -avz --delete --exclude-from 'rsync-exclude.txt' src/ /Users/loco/dev/CushyStudio/src/FlexLayout/
-rsync -avz --delete --exclude-from 'rsync-exclude.txt' style/ /Users/loco/dev/CushyStudio/src/FlexLayout/style
+mkdir -p /Users/loco/dev/CushyStudio/src/FlexLayout/style
+rsync style/dark.css /Users/loco/dev/CushyStudio/src/FlexLayout/style/dark.css
 rsync LICENSE /Users/loco/dev/CushyStudio/src/FlexLayout/LICENSE
 rsync .prettierrc.json /Users/loco/dev/CushyStudio/src/FlexLayout/.prettierrc.json
 echo "Synced with CushyStudio"
